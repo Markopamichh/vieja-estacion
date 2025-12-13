a796056d9eb15bf0913c1c0e1ed3344be323be88
Markopamichh <markopamich@gmail.com>
2025-12-12 21:21:35 -0300

    varios

--- vieja-estacion-astro/astro.config.mjs
+++ vieja-estacion-astro/astro.config.mjs
@@ -1,9 +1,5 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
-import sitemap from '@astrojs/sitemap';
 
-// https://astro.build/config
 export default defineConfig({
-  output: 'static',
-  integrations: [sitemap()]
+  output: 'static'
 });
